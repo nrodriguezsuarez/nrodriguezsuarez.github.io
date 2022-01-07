9364b8d047b4a052e93f826cf1741528f8f1dafc
Néstor Rodríguez Suárez <nestor.rodriguez-suarez@comeco.com>
2022-01-07 13:55:45 +0100

    Nase JS file Aadded

---
+++ index.js
@@ -0,0 +1,10 @@
+let D = document;
+let $privateKey = D.getElementById('id_privateKey');
+let $publicKey = D.getElementById('id_publicKey');
+let $resultPlain = D.getElementById('id_resultPlain');
+let $resultUnencripted = D.getElementById('id_resultUnencrypted');
+
+function RequestData ()
+{
+
+}
